657c839e740d44b9b73ff9a1e9bcd6aa13f76528
JannuIba17 <jannath.sarahibrat>
2025-10-28 17:35:35 +0530

    feat: Updated HelloWorld.java File

--- JavaLearning/src/com/Day1/HelloWorld.java
+++ JavaLearning/src/com/Day1/HelloWorld.java
@@ -1,8 +1,14 @@
-package com.Basics;
+package com.Day1;
 
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World!");
         System.out.println("This is the line from github");
+//        System.err.println("Error Message!");
+        System.out.println("This is the last line");
+        String name="Jannath";
+        int age=31;
+        System.out.printf("My Name is %s%nMy Age is %d",name,age);
     }
 }
+
